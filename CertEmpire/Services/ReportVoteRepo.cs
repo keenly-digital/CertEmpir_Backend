f1d88258f2b4f2e8e5e4cd94d59e559ddfb0fa1f
Hamza Umar <94223269+Hamzify@users.noreply.github.com>
2025-07-02 01:15:44 +0500

    changes

--- CertEmpire/Services/ReportVoteRepo.cs
+++ CertEmpire/Services/ReportVoteRepo.cs
@@ -42,7 +42,7 @@ namespace CertEmpire.Services
                 {
                     int pageSize = request.PageNumber * 10; // Assuming PageSize is 10
                     //getting all the tasks
-                    var tasks = await _context.Reports.Take(pageSize).ToListAsync();
+                    var tasks = await _context.Reports.Take(pageSize).OrderByDescending(x=>x.Created).ToListAsync();
                     if (tasks.Any())
                     {
                         foreach (var item in tasks)
